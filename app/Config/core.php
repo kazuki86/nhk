18de5d9e4964bda5ebaa81a760133b611f4ff788
k-masuda <k-masuda@www8030up.sakura.ne.jp>
2015-01-26 22:39:15 +0900

    make all green.

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'guVoUfIxfs2WwvniR2G0FubDYhG93b0qyJgaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76354249856749594683643096575');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
