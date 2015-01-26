3d2ffad06bd591a2390946f4d11448f6f1655585
k-masuda <k-masuda@www8030up.sakura.ne.jp>
2015-01-26 23:06:21 +0900

    NHK APIを使った簡単なサンプルの実装。
    Topコントローラとindexを追加。
    NHK APIにリクエストを発行して取得したjsonを画面にただ表示する。
    API KEYはGitで晒したくないのでApacheのSetEnvで指定するようにしておく

---
+++ app/Controller/TopController.php
@@ -0,0 +1,26 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class TopController extends AppController {
+
+  public function index() {
+
+    $API_KEY_NHK = $this->getNhkApiKey();
+    $today = date('Y-m-d');
+
+    //sample query for NHK API
+    $list_json = file_get_contents("http://api.nhk.or.jp/v1/pg/list/130/tv/{$today}.json?key={$API_KEY_NHK}");
+    $list = json_decode($list_json, true);
+
+		$this->set('list', $list['list']);
+  }
+
+  private function getNhkApiKey() {
+    $key = $_SERVER['NHK_API_KEY'];
+    if (empty($key)) {
+      throw new RuntimeException('The Apache Env: NHK_API_KEY is missing.');
+    }
+    return  $key;
+  }
+}
+
