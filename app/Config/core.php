7eda9d79e302dddfd884e2965572631aa008d83a
k-masuda <k-masuda@www8030up.sakura.ne.jp>
2015-02-07 21:26:30 +0900

    fixed #13 debugモードを環境ごとに自動判別。
    Apacheの設定でDEBUG_LEVELを１か２に設定しておけばデバッグモードになる。
    基本的には開発環境で必要に応じて定義するだけ。本番では何も設定しなければよい。

--- app/Config/core.php
+++ app/Config/core.php
@@ -31,7 +31,11 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+if (env('DEBUG_LEVEL')>0) {
+  Configure::write('debug', env('DEBUG_LEVEL'));
+} else {
+  Configure::write('debug', 0);
+}
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
