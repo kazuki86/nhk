0e69f2a6f248232802a232a62b7139c3003399d9
k-masuda <k-masuda@www8030up.sakura.ne.jp>
2015-02-07 23:02:42 +0900

    fixed #10 タイムゾーンの設定をする。

--- app/Config/core.php
+++ app/Config/core.php
@@ -271,7 +271,7 @@ if (env('DEBUG_LEVEL')>0) {
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+	date_default_timezone_set('Asia/Tokyo');
 
 /**
  * `Config.timezone` is available in which you can set users' timezone string.
